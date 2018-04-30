7c55724ed79a41a40bfccc3157f7540c7ef8fc11
Alexis <gavidia.alex@gmail.com>
2018-04-30 12:13:40 -0500

    add html structure and dom js variables.

---
+++ number8.js
@@ -0,0 +1,11 @@
+
+var dom_days = document.getElementById("npt-days");
+var dom_startDate = document.getElementById("npt-start-date");
+var dom_codeCountry = document.getElementById("npt-country-code");
+var dom_btn = document.getElementById("btn-generate");
+var dom_pnl = document.getElementById("pnl-calendar");
+
+dom_btn.addEventListener("click", function () {
+     // do something
+}, false);
+ 
