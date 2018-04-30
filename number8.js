94515a07f5ae749373121328c6692106cb8b420f
Alexis <gavidia.alex@gmail.com>
2018-04-30 12:37:26 -0500

    add a dictionary with month names indexed by their number

--- number8.js
+++ number8.js
@@ -6,26 +6,41 @@ var dom_btn = document.getElementById("btn-generate");
 var dom_pnl = document.getElementById("pnl-calendar");
 
 
-dom_btn.addEventListener("click", function () { 
+dom_btn.addEventListener("click", function () {
     var startDate = new Date(dom_startDate.value); // set the start date
     var days_npt = dom_days.value; // set count days
- 
+
     // testing calendar
- 
+
 }, false);
-  
-function buildCalendar( ) {
-    
-    return ; // return the calendar per month
+
+var dic_months = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December"
+}
+
+function buildCalendar() {
+
+    return; // return the calendar per month
 }
 
 
 //build all dom calendar represented as table
-function builDomCalendar(title){
-    
-    var tbl = document.createElement('table'); 
-    tbl.className = "ui celled small structured unstackable table" 
-    var thead = document.createElement('thead'); 
+function builDomCalendar(title) {
+
+    var tbl = document.createElement('table');
+    tbl.className = "ui celled small structured unstackable table"
+    var thead = document.createElement('thead');
     var tr = document.createElement('tr');
     var th = document.createElement('th');
     th.innerHTML = "S";
